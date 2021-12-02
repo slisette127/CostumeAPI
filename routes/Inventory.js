4d58d4abf37244e369bcb6ffda9dbb7031c7e984
unknown <slisette127@gmail.com>
2021-12-01 21:58:45 -0600

    clean up

--- routes/Inventory.js
+++ routes/Inventory.js
@@ -1,7 +1,7 @@
-const { Router } = require("express");
 const express = require("express");
 const router = express.Router()
 const inventoryController = require("../controller/Inventory")
+
 router.get("/inventory", inventoryController.getInventory)
 router.post("/addItem", inventoryController.addItem)
 
