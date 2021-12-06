686bca1854853dc95173c13364eed5e08bd6358f
unknown <slisette127@gmail.com>
2021-12-05 22:01:08 -0600

    final push

--- routes/User.js
+++ routes/User.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const userController = require("../controller/User")
+
 router.post("/adduser", userController.newUser, userController.newUserInventory, userController.newUserProject)
 router.post("/login", userController.logIn) 
 
